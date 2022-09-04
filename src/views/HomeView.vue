b5712b41faa757344b58fcb802911f931779aebf
lukachochua <lukachochua@gmail.com>
2022-09-04 16:51:22 +0400

    Episode 19: Composition Api, script setup and reactivity

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup>
 import TheWelcome from "@/components/TheWelcome.vue";
+
+
+let message = $ref("Hello World!");
+
+let doSomething = () => {
+  alert("Doing it now");
+};
+
+
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+
+    <p>{{ message }}</p>
+
+
+    <p>
+      <button @click="doSomething">Click me</button>
+    </p>
   </main>
 </template>
