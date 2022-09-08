46b380cc5a33a89ce5a3ec26ec3b3baa33c047ed
lukachochua <lukachochua@gmail.com>
2022-09-08 22:55:00 +0400

    episode 24: State, Actions and mutating state

---
+++ src/views/CounterView.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>{{ counter.count }}</h1>
+        <button @click="counter.increment()">Increment</button>
+    </div>
+</template>
+
+<script setup>
+    import {counter} from "@/stores/counterStore.js";
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,9 @@
 <script setup>
-import TabbableTextarea from "@/components/TabbableTextarea.vue";
-import { ref } from "vue";
-
-let comment = ref('test value');
-
-setTimeout(()=> {
-  comment.value = "It Works";
-}, 2000); 
 
 </script>
 
 <template>
   <main>
-    <form action="">
-      <TabbableTextarea v-model="comment" style="width:100%; height:300px;" />
-    </form>
+    
   </main>
 </template>
