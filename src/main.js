22eafdf7853f32607ee09351d91238273570b8c5
lukachochua <lukachochua@gmail.com>
2022-09-12 14:31:42 +0400

    feat: extract components, create fake db as a json file

--- src/main.js
+++ src/main.js
@@ -3,12 +3,11 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import "./assets/main.css";
+import './assets/index.css'
 
 const app = createApp(App);
 
 app.use(router);
-
 app.use(createPinia());
 
 app.mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,15 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ContactView from "../views/ContactView.vue";
 import CounterView from "../views/CounterView.vue";
+import TeamView from "../views/TeamView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
+      name: "team",
+      component: TeamView,
     },
     {
       path: "/about",
